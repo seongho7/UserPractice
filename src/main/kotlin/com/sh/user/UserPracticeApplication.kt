306290e54b54bbbe6f717566681687b168478505
하성호 <coanny@naver.com>
2022-09-03 21:27:14 +0900

    init

---
+++ src/main/kotlin/com/sh/user/UserPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.sh.user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/sh/user/UserPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sh.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
