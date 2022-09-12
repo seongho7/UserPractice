dedbb917310d3b40e7fe80aac4f757a413f474c9
하성호 <coanny1@gmail.com>
2022-09-12 22:57:19 +0900

    bug fix

--- src/main/kotlin/com/sh/user/cellphoneverification/domain/send/TokenGenerator.kt
+++ src/main/kotlin/com/sh/user/cellphoneverification/domain/send/TokenGenerator.kt
@@ -3,13 +3,12 @@ package com.sh.user.cellphoneverification.domain.send
 import java.util.concurrent.ThreadLocalRandom
 
 class TokenGenerator {
-    private val threadLocalRandom = ThreadLocalRandom.current()
     companion object {
         const val TOKEN_MIN = 100000
         const val TOKEN_MAX = 999999
     }
 
     fun nextInt() : Int {
-        return threadLocalRandom.nextInt(TOKEN_MAX- TOKEN_MIN) + TOKEN_MIN
+        return ThreadLocalRandom.current().nextInt(TOKEN_MAX- TOKEN_MIN) + TOKEN_MIN
     }
 }
